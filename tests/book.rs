19b43c074dd68759b1e093c5bed86ac14f1c6aca
Fauconfan <joseph0priou@gmail.com>
2022-07-09 13:22:00 +0200

    add mdbook as depedency in CI tests

--- tests/book.rs
+++ tests/book.rs
@@ -2,9 +2,6 @@ use std::process::Command;
 
 #[test]
 fn build_test_book() {
-    let ls = Command::new("ls").arg("-lR").output().unwrap();
-    println!("{:?}", String::from_utf8_lossy(&ls.stdout).to_string());
-
     let output = Command::new("mdbook")
         .arg("build")
         .current_dir("./tests/book/")
