73005a7bce084d522bb8332bd956a0e76553b0ba
Fauconfan <joseph0priou@gmail.com>
2022-07-09 13:14:42 +0200

    test build book in CI

--- tests/book.rs
+++ tests/book.rs
@@ -2,9 +2,12 @@ use std::process::Command;
 
 #[test]
 fn build_test_book() {
+    let ls = Command::new("ls").arg("-lR").output().unwrap();
+    println!("{:?}", String::from_utf8_lossy(&ls.stdout).to_string());
+
     let output = Command::new("mdbook")
         .arg("build")
-        .current_dir("./tests/book")
+        .current_dir("./tests/book/")
         .output()
         .unwrap();
 
