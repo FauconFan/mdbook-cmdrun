86f341f42eaa6388550504305cda5a8ea98842c3
Exsjabe <jakob.bergstrom@explizit.se>
2023-06-27 16:16:37 +0200

    Started fixing tests for windows

--- tests/command.rs
+++ tests/command.rs
@@ -1,3 +1,4 @@
+use cfg_if::cfg_if;
 use mdbook_cmdrun::CmdRun;
 
 macro_rules! add_test {
@@ -10,21 +11,30 @@ macro_rules! add_test {
         }
     };
 }
-
-add_test!(simple1, "echo oui", "oui\n");
-add_test!(simple2, "echo oui non", "oui non\n");
-add_test!(simple3, "echo oui       non", "oui non\n");
-add_test!(simple4, "echo oui; echo non", "oui\nnon\n");
-add_test!(simple5, "echo \"hello world\"", "hello world\n");
-add_test!(pipe1, "cat LICENSE | head -n 1", "MIT License\n");
-add_test!(pipe2, "yes 42 | head -n 3", "42\n42\n42\n");
-add_test!(pipe3, "echo \" coucou   \" | tr -d ' '", "coucou\n");
-add_test!(quote1, "echo \"\"", "\n");
-add_test!(quote2, "echo \"\\\"\"", "\"\n");
-add_test!(quote3, "echo ''", "\n");
-add_test!(quote4, "echo '\\'", "\\\n");
+add_test!(simple1, "echo oui", "oui");
+add_test!(simple2, "echo oui non", "oui non");
+add_test!(pipe1, "cat LICENSE | head -n 1", "MIT License");
+add_test!(pipe2, "yes 42 | head -n 3", "42\n42\n42");
 add_test!(
     mixed1,
     "yes 42 | head -n 4 | sed -z 's/\\n/  \\n/g'",
-    "42  \n42  \n42  \n42  \n"
+    "42  \n42  \n42  \n42"
 );
+
+cfg_if! {
+    if #[cfg(any(target_family = "unix", target_family = "other"))] {
+        add_test!(simple3, "echo oui       non", "oui non");
+        add_test!(simple4, "echo oui; echo non", "oui\nnon");
+        add_test!(simple5, "echo \"hello world\"", "hello world");
+        add_test!(pipe3, "echo \" coucou   \" | tr -d ' '", "coucou");
+        add_test!(quote1, "echo \"\"", "");
+        add_test!(quote2, "echo \"\\\"\"", "\"");
+        add_test!(quote3, "echo ''", "");
+        add_test!(quote4, "echo '\\'", "\\");
+    } else if #[cfg(target_family = "windows")] {
+        add_test!(simple3, "echo oui       non", "oui       non");
+        add_test!(simple4, "echo oui& echo non", "oui\r\nnon");
+        add_test!(simple5, "echo hello world", "hello world");
+        add_test!(pipe3, "echo  coucou    | tr -d ' '", "coucou");
+    }
+}
