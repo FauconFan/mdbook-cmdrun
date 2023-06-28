ef5eb511e285ac0557c4022e1e1dcc39548ae4b7
Exsjabe <jakob.bergstrom@explizit.se>
2023-06-28 13:53:28 +0200

    Clippy fix

--- tests/regression.rs
+++ tests/regression.rs
@@ -62,7 +62,6 @@ macro_rules! add_dir {
 fn check_all_regressions_dirs() {
     let mut entries: Vec<String> = fs::read_dir("./tests/regression")
         .unwrap()
-        .into_iter()
         .map(|r| r.unwrap())
         .map(|de| String::from(de.file_name().to_string_lossy()))
         .collect();
